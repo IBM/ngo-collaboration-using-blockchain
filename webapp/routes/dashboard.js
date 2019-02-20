039d2d0fb6e35ccf2b05268e110523f2a129859a
Muralidhar Chavan <muralidhar.chavan@in.ibm.com>
2019-02-20 14:44:13 +0530

    local data

--- webapp/routes/dashboard.js
+++ webapp/routes/dashboard.js
@@ -4,8 +4,12 @@ var Request = require("request");
 
 /* GET dashboard page. */
 router.get('/', function (req, res, next) {
-  var data={};
-
+  //var data={};
+  var data= [{"id":1, "created_by": "User1", "created_date": "20-Dec-2018", "category": "Cat1", "quantity_required": 100, "quantity_remaining": 50, "expected_date_of_fullfillment": "30-Jan-2019"},
+  {"id":2, "created_by": "User2", "created_date": "15-Dec-2018", "category": "Cat2", "quantity_required": 500, "quantity_remaining": 100, "expected_date_of_fullfillment": "15-Jan-2019"},
+  {"id":3, "created_by": "User1", "created_date": "10-Dec-2018", "category": "Cat3", "quantity_required": 300, "quantity_remaining": 250, "expected_date_of_fullfillment": "20-Jan-2019"}
+  ];
+/*
   Request.get("http://localhost:8000/rest/allneeds", (error, response, body) => {
     if (error) {
       return console.dir(error);
@@ -14,6 +18,8 @@ router.get('/', function (req, res, next) {
     console.dir(data);
     res.render('dashboard', { needs: data });
   });
+  */
+ res.render('dashboard', { needs: data });
 });
 
 module.exports = router;
--- webapp/routes/needdetails.js
+++ webapp/routes/needdetails.js
@@ -4,8 +4,13 @@ var Request = require("request");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var data={};
-
+    //var data={};
+    var data=[
+      { "pledgeid": 1, "qty": 10, "user": "user1" },
+      { "pledgeid": 2, "qty": 20, "user": "user2" },
+      { "pledgeid": 3, "qty": 22, "user": "user3" }
+  ];
+/*
     Request.get("http://localhost:8000/rest/needdetails", (error, response, body) => {
       if (error) {
         return console.dir(error);
@@ -14,6 +19,8 @@ router.get('/', function (req, res, next) {
       console.dir(data);
       res.render('needdetails', { pledgesdata: data });
     });
+    */
+   res.render('needdetails', { pledgesdata: data });
 });
 
 module.exports = router;
