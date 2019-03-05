05b104d18f8705b83acf6eb43da96fdb3042f289
Muralidhar Chavan <muralidhar.chavan@in.ibm.com>
2019-03-05 15:00:39 +0530

    link create Need and dashboard rest integration

--- webapp/routes/dashboard.js
+++ webapp/routes/dashboard.js
@@ -4,22 +4,33 @@ var Request = require("request");
 
 /* GET dashboard page. */
 router.get('/', function (req, res, next) {
-  //var data={};
+  var data=[];
+  /*
   var data= [{"id":1, "created_by": "User1", "created_date": "20-Dec-2018", "category": "Cat1", "quantity_required": 100, "quantity_remaining": 50, "expected_date_of_fullfillment": "30-Jan-2019"},
   {"id":2, "created_by": "User2", "created_date": "15-Dec-2018", "category": "Cat2", "quantity_required": 500, "quantity_remaining": 100, "expected_date_of_fullfillment": "15-Jan-2019"},
   {"id":3, "created_by": "User1", "created_date": "10-Dec-2018", "category": "Cat3", "quantity_required": 300, "quantity_remaining": 250, "expected_date_of_fullfillment": "20-Jan-2019"}
   ];
-/*
-  Request.get("http://localhost:8000/rest/allneeds", (error, response, body) => {
+  */
+  ///*
+  var url = "https://ngo-collaboration-java-app-friendly-numbat.mybluemix.net/QueryAllNeedsServlet";
+  var body = { "function": "queryAllNeeds" };
+
+  Request.post({
+    url: url,
+    body: body,
+    json: true
+  }, function (error, response, body) {
     if (error) {
       return console.dir(error);
     }
-    data = JSON.parse(body);
-    console.dir(data);
+    console.log("Response body = " + JSON.stringify(body));
+    console.log("record = " + JSON.stringify(body[0].Record));
+    body.forEach(element => {
+      data.push(element.Record);
+    });
+    console.log("Rearranged data = " + JSON.stringify(data));
     res.render('dashboard', { needs: data });
   });
-  */
- res.render('dashboard', { needs: data });
 });
 
 module.exports = router;
