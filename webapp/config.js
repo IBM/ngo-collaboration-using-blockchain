3aef70eb5729b283d2dc26289a885b5e8ec3a9ca
Muralidhar Chavan <muralidhar.chavan@in.ibm.com>
2019-03-11 21:22:10 +0530

    integrate pastneeds

--- webapp/config.js
+++ webapp/config.js
@@ -1,7 +1,13 @@
 var config = {};
 
-config.rest_base_url = 'http://ngo-collaboration-java-app-friendly-numbat.mybluemix.net';
+// config.rest_base_url = 'http://ngo-collaboration-java-app-friendly-numbat.mybluemix.net';
 
-config.rest_hostname = 'ngo-collaboration-java-app-friendly-numbat.mybluemix.net';
+// config.rest_hostname = 'ngo-collaboration-java-app-friendly-numbat.mybluemix.net';
+
+// https://ngo-collaboration-java-app.mybluemix.net/ 
+
+config.rest_base_url = 'http://ngo-collaboration-java-app-wacky-genet.mybluemix.net';
+
+config.rest_hostname = 'ngo-collaboration-java-app-wacky-genet.mybluemix.net';
 
 module.exports = config;
